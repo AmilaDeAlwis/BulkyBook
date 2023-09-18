32f4ec8d3c78d9713eeb0d64758f5d81c796b203
AmilaDeAlwis <amilaalwis4@gmail.com>
2023-09-18 20:22:48 +0530

    created categories can be POST to DB

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -23,5 +23,15 @@ namespace BulkyBookWeb.Controllers
         {
             return View();
         }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
